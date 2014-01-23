b3003e873ddb3468ac115e82ea5326cfc2bc6389
Michael Johnson <mjohnson@redhat.com>
2014-01-23 14:06:49 -0500

    adding more excercise files

---
+++ ex11-study-drills.py
@@ -0,0 +1,6 @@
+print "What's your favorite movie?",
+movie = raw_input()
+print "How old are you?",
+age = raw_input()
+print "So, your favorite move is %r and you're %r years old?" % (
+    movie, age)
---
+++ ex13.py
@@ -0,0 +1,13 @@
+from sys import argv
+
+script, first, second, third = argv
+
+print "This script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
+
+input1 = raw_input("Gimme somethin' for input 1: ")
+input2 = raw_input("Gimme somethin' for input 2: ")
+
+# python ex13.py first 2nd 3rd
